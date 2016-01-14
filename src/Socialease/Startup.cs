79767a45be04655f697149901ecaba02c7860889
dejital <sergey@sergeyk.com>
2016-01-13 20:30:06 -0600

    Remove references to IIS.

--- src/Socialease/Startup.cs
+++ src/Socialease/Startup.cs
@@ -20,11 +20,9 @@ namespace Socialease
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync($"Hello from {context.Request.Path}!");
             });
         }
 
