caf6abb3646be6539b75ced2c2a13e6b993bb60c
dejital <sergey@sergeyk.com>
2016-01-13 21:59:49 -0600

    Include some js.

---
+++ src/Socialease/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// site.js
+(function() {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Serge Nevsky";
+    ele.onmouseenter = function() {
+        ele.style["color"] = "green";
+    };
+    ele.onmouseleave = function() {
+        ele.style["color"] = "";
+    }
+})();
