a9013356c221a2a5785a9c6caf50cc85100908cb
dejital <sergey@sergeyk.com>
2016-02-01 22:20:21 -0600

    Login view and auth controller

---
+++ src/Socialease/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Socialease.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("People", "App");
+            }
+
+            return View();
+        }
+    }
+}
