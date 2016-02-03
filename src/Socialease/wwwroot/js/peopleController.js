595aa6d79e929691944714d0c3f79e259d524f9c
dejital <sergey@sergeyk.com>
2016-02-03 11:57:26 -0600

    Add angular with peopleController

---
+++ src/Socialease/wwwroot/js/peopleController.js
@@ -0,0 +1,28 @@
+﻿(function() {
+
+	'use strict';
+
+    angular.module('app')
+        .controller('peopleController', peopleController);
+
+	function peopleController() {
+
+		var vm = this;
+
+	    vm.people = [
+	        {
+	        	name: 'John Fante',
+				lastPing: new Date()
+	        }, {
+	            name: 'Arturo Bandini',
+				lastPing: new Date()
+	        }
+	    ];
+
+		vm.addPerson = function() {
+		    vm.people.push({ name: vm.newPerson.name, lastPing: new Date() });
+		}
+
+	}
+
+})();
