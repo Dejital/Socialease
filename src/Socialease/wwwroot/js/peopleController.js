b26c639e2ccc4b93f3b88c3ddab060e53b3157f1
dejital <sergey@sergeyk.com>
2016-03-08 21:58:43 -0600

    Add filter to person list

--- src/Socialease/wwwroot/js/peopleController.js
+++ src/Socialease/wwwroot/js/peopleController.js
@@ -12,7 +12,10 @@
 	    vm.people = [];
 	    vm.newPerson = {};
 	    vm.errorMessage = '';
+	    vm.nameFilter = '';
 	    vm.isBusy = true;
+	    vm.isFilterSeen = false;
+	    vm.selectedPerson = { value: 0 };
 
 	    $http.get('/api/people')
 	        .then(function(response) {
@@ -23,6 +26,10 @@
 	        .finally(function() {
 	            vm.isBusy = false;
 	        });
+
+	    vm.toggleFilter = function() {
+	        vm.isFilterSeen = !vm.isFilterSeen;
+	    };
 
 		vm.addPerson = function() {
 		    vm.isBusy = true;
